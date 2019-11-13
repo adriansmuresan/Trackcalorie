228d9a6c0e0928e09519f00b6cb56f6436ce17e2
Adrian S MUresan <adriansmuresan@gmail.com>
2019-11-13 15:29:15 -0600

    Removes dummy data, creates a hideList function that will hide the ul bar that was present in the UI if no items were in the list

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const ItemCtrl = (function() {
   // Data Structure/ State
   const data = {
     items: [
-      {id: 0, name: 'Steak Dinner', calories: 1200},
-      {id: 1, name: 'Cookie', calories: 400},
-      {id: 2, name: 'Eggs', calories: 300},
+      // {id: 0, name: 'Steak Dinner', calories: 1200},
+      // {id: 1, name: 'Cookie', calories: 400},
+      // {id: 2, name: 'Eggs', calories: 300},
 
     ],
     currentItem: null,
@@ -88,6 +88,8 @@ const UICtrl = (function() {
       }
     },
     addListItem: function(item) {
+      // Show the list
+      document.querySelector(UISelectors.itemList).style.display = 'block';
       // Create li element
       const li = document.createElement('li');
       // Add class
@@ -107,6 +109,9 @@ const UICtrl = (function() {
       document.querySelector(UISelectors.itemNameInput).value = '';
       document.querySelector(UISelectors.itemCaloriesInput).value = '';
 
+    },
+    hideList: function() {
+      document.querySelector(UISelectors.itemList).style.display = 'none';
     },
     getSelectors: function() {
       return UISelectors;
@@ -151,10 +156,15 @@ const App = (function(ItemCtrl, UICtrl) {
     init: function() {
       // Fetch items from data structure
       const items = ItemCtrl.getItems();
-      
+
+      // Check if any items
+      if(items.length === 0) {
+        UICtrl.hideList();
+      } else {
       // Populate list with items
       UICtrl.populateItemsList(items);
-
+      }
+      
       // Load event listeners
       loadEventListeners();
     }
