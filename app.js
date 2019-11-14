fa9e98a2e6a86c5596fb7f05533d1a4d0fa8c5d6
Adrian S MUresan <adriansmuresan@gmail.com>
2019-11-14 15:20:12 -0600

    Defines the itemUpdateSubmit function by getting the list id, splitting it into an array and getting the actual id

--- app.js
+++ app.js
@@ -190,7 +190,17 @@ const App = (function(ItemCtrl, UICtrl) {
   // Update item submit
   const itemUpdateSubmit = function(e) {
     if(e.target.classList.contains('edit-item')) {
-      
+      // Get list item id (item-0, item-1)
+      const listId = e.target.parentNode.parentNode.id;
+
+      // Break into an array
+      const listIdArr = listId.split('-');
+
+      // Get the actual id
+      const id = parseInt(listIdArr[1]);
+
+      // Get item
+      const itemToEdit = ItemCtrl.getItemById(id);
     }
     e.preventDefault();
   }
