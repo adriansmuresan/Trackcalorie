22a78e79208c3668e9f41ab92c605b592c22f4b7
Adrian S MUresan <adriansmuresan@gmail.com>
2019-11-19 16:00:31 -0600

    Adds the items to the data structure by calling the getItemsfromStorage on the storage controler

--- app.js
+++ app.js
@@ -47,12 +47,13 @@ const ItemCtrl = (function() {
 
   // Data Structure/ State
   const data = {
-    items: [
-      // {id: 0, name: 'Steak Dinner', calories: 1200},
-      // {id: 1, name: 'Cookie', calories: 400},
-      // {id: 2, name: 'Eggs', calories: 300},
+    // items: [
+    //   // {id: 0, name: 'Steak Dinner', calories: 1200},
+    //   // {id: 1, name: 'Cookie', calories: 400},
+    //   // {id: 2, name: 'Eggs', calories: 300},
 
-    ],
+    // ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0
   }
