554233cc18beecd4c16fe9d1ef307342adb6687d
Adrian S MUresan <adriansmuresan@gmail.com>
2019-11-20 14:34:08 -0600

    Adds the clearItemsFromStorage function

--- app.js
+++ app.js
@@ -53,6 +53,9 @@ const StorageCtrl = (function(){
       });
       localStorage.setItem('items', JSON.stringify(items));      
     },
+    clearItemsFromStorage: function(){
+      localStorage.removeItem('items');
+    },
   }
 })();
 
@@ -439,6 +442,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 
     // Remove from UI
     UICtrl.removeItems();
+
+    // Clear from local storage
+    StorageCtrl.clearItemsFromStorage();
 
     // Hide UL
     UICtrl.hideList();
